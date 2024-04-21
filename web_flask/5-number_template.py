5673effdd2e52c9505b00f72cc59e882547b8600
tantohtelmah <tantohtelmah@gmail.com>
2024-04-20 22:26:49 -0400

    tasks

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -10,16 +10,19 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """ prints a display"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
+    """ prints a display"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def display_custom_text(text):
+    """ prints a formatted text"""
     formatted_text = text.replace('_', ' ')
     return f"C {formatted_text}"
 
@@ -27,17 +30,20 @@ def display_custom_text(text):
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def display_python_text(text):
+    """ prints a formatted text"""
     formatted_text = text.replace('_', ' ')
     return f"Python {formatted_text}"
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def display_number(n):
+    """ prints a number"""
     return f"{n} is a number"
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def display_number_template(n):
+    """ prints an htlm page """
     return render_template('5-number.html', number=n)
 
 
