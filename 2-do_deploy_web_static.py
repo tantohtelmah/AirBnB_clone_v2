d633636a1eafc9edf29a7d0b20b9a9fa645c460c
tantohtelmah <tantohtelmah@gmail.com>
2024-04-20 23:31:09 -0400

    tasks

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """
 Fabric script based on the file 1-pack_web_static.py that distributes an
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """
 Fabric script based on the file 2-do_deploy_web_static.py that creates and
