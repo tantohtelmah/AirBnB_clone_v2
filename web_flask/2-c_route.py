ff010f80da59c86a6850b098f30d989352cf32bd
tantohtelmah <tantohtelmah@gmail.com>
2024-04-20 21:40:09 -0400

    tasks

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Installed and utilising flask for a dynamic feel
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_custom_text(text):
+    # Replace underscores with spaces
+    formatted_text = text.replace('_', ' ')
+    return f"C {formatted_text}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
