fe77f0cfd5d26a170aaa95140d66722d160762e8
tantohtelmah <tantohtelmah@gmail.com>
2024-04-20 22:21:45 -0400

    tasks

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Installed and utilising flask for a dynamic feel
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_custom_text(text):
+    formatted_text = text.replace('_', ' ')
+    return f"C {formatted_text}"
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python_text(text):
+    formatted_text = text.replace('_', ' ')
+    return f"Python {formatted_text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def display_number_template(n):
+    return render_template('5-number.html', number=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
